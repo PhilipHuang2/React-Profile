0f66ceb528d9cf62197bf1929d619ab918d9b627
Philip Huang <philiphuang2@gmail.com>
2022-12-04 21:15:51 -0800

    updated the spacing in the footer

--- react-profile/src/Component/Footer.js
+++ react-profile/src/Component/Footer.js
@@ -7,13 +7,14 @@ export default function Footer() {
   return (
     <Container>
       <footer className="py-3 my-4">
-        <ul className="nav justify-content-center border-bottom pb-3 mb-3">
+        <p className="text-center">Check me out online!</p>
+        <ul className="nav justify-content-center border-bottom pb-3 mb-3 d-flex align-content-center">
           <li className="nav-item">
             <a href="https://github.com/PhilipHuang2">
               <img src={GitHub} alt="Link to my GitHub Profile"/>
             </a>
           </li>
-          <li className="nav-item">
+          <li className="nav-item mx-5">
             <a href="https://www.linkedin.com/in/philip-h-6a4b569b/">
               <img src={Linkedin} alt="Link to my LinkedIn Profile"/>
             </a>
@@ -24,7 +25,7 @@ export default function Footer() {
             </a>
           </li>
         </ul>
-        <p className="text-center tex-muted">© 2022 Company, Inc</p>
+        <p className="text-center tex-muted">© 2022 Philip Huang</p>
       </footer>
     </Container>
   );
