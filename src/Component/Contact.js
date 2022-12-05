f63274088294e721a235560026873f073f680614
Philip Huang <philiphuang2@gmail.com>
2022-12-05 00:39:10 -0800

    added basic form state management

--- src/Component/Contact.js
+++ src/Component/Contact.js
@@ -1,5 +1,48 @@
-export default function AboutMe(){
-    return(
-        <p>This for my contacts info</p>
-    )
+import { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+export default function AboutMe() {
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [message, setMessage] = useState("");
+
+  const updateInputChange = (e) => {
+    const { target } = e;
+    const inputType = target.name;
+    const inputValue = target.value;
+    if (inputType === 'email') {
+        setEmail(inputValue);
+      } else if (inputType === 'name') {
+        setName(inputValue);
+      } else {
+        setMessage(inputValue);
+      }
+      console.log(email, name, message)
+  };
+  return (
+    <div>
+      <p style={{textAlign: "center", fontSize:"20px"}}>Send me an Email!</p>
+      <Form className="px-2"style={{marginRight:"25%", marginLeft:"25%", backgroundColor: "lightgrey"}}>
+        <Form.Group className="mb-3" controlId="formName">
+          <Form.Label>Name</Form.Label>
+          <Form.Control type="text" placeholder="Enter your name" name="name" onChange={updateInputChange} />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter your email" name="email" onChange={updateInputChange} />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formMessage">
+          <Form.Label>Message</Form.Label>
+          <Form.Control as="textarea" rows={3}  name="message" onChange={updateInputChange}/>
+        </Form.Group>
+
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </div>
+  );
 }
