7fa2b6b1297ef73421165a2a5406bdcf230a3a6c
Philip Huang <philiphuang2@gmail.com>
2022-12-05 22:52:46 -0800

    almost done

--- src/Component/Project.js
+++ src/Component/Project.js
@@ -2,7 +2,7 @@ import GitHub from './images/GitHub.png';
 
 export default function Project(props) {
   return (
-    <div className="card mx-auto my-2" style={{ width: "30rem" }}>
+    <div className="card mx-auto my-2 mb-4 border-secondary border-5 border" style={{ width: "30rem", borderRadius: "5px" }}>
       <img className="class-img-top" src={props.img} alt="dog rater website" />
       <div className="card-body">
         <h3 className="text-center card-title">{props.title}</h3>
--- src/Files/projects.js
+++ src/Files/projects.js
@@ -1,4 +1,9 @@
 import DogRater from "../Component/images/DogRater.PNG";
+import Password from "../Component/images/password.png"
+import ParticleProblems from "../Component/images/ParticleProblems.PNG"
+import WeatherDashboard from "../Component/images/WeatherDashboard.PNG"
+import WorkDayScheduler from "../Component/images/WorkDayScheduler.png"
+import Quiz from "../Component/images/Quiz.PNG"
 
 const projects = [
     {
@@ -6,6 +11,37 @@ const projects = [
         img: DogRater,
         link: "https://fetchdograter.herokuapp.com/",
         github: "https://github.com/mseaman26/Fetch"
+    },
+    {
+        title: "Humane Password Generator",
+        img: Password,
+        link: "https://github.com/PhilipHuang2/Humane-Password-Generator/",
+        github: "https://philiphuang2.github.io/Humane-Password-Generator/"
+    },
+    {
+        title: "Particle Problems",
+        img: ParticleProblems,
+        link: "https://duffylaura.github.io/Particle-Problems/",
+        github: "https://github.com/duffylaura/Particle-Problems"
+    },
+    {
+        title: "Weather Dashboard",
+        img: WeatherDashboard,
+        link: "https://philiphuang2.github.io/Weather-Dashboard/",
+        github: "https://github.com/PhilipHuang2/Weather-Dashboard/settings/pages"
+
+    },
+    {
+        title: "Work Day Scheduler",
+        img: WorkDayScheduler,
+        link: "https://philiphuang2.github.io/Work-Day-Calendar/",
+        github: "https://github.com/PhilipHuang2/Work-Day-Calendar"
+    },
+    {
+        title: "Javascript Quiz",
+        img: Quiz,
+        link: "https://philiphuang2.github.io/Web-Quiz/",
+        github: "https://github.com/PhilipHuang2/Web-Quiz/settings/pages"
     }
 ]
 
